738c09491c2f379332a9eaca0bca09759ad84343
Igor <izelenov7@mail.ru>
2025-05-05 19:17:03 +0300

    add reload page after game end

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let result = 1;
 
-document.querySelector('.question').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
   console.log(guessingNumber);
+
+  document.querySelector('.again').addEventListener('click', () => {
+    location.reload();
+  });
 
   if (!guessingNumber) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
   } else if (guessingNumber === secretNumber) {
     document.querySelector('.guess-message').textContent = 'Вы угадали!';
+    document.querySelector('.question').textContent = secretNumber;
     document.querySelector('.highscore').textContent = result;
     document.querySelector('.question').style.backgroundColor = 'green';
     document.querySelector('header').style.borderBottom = '7px solid green';
-    document.querySelector('h1').textContent = 'Победа!'
+    document.querySelector('h1').textContent = 'Победа!';
+    document.querySelector('h1').style.fontSize = '6rem';
   } else if (guessingNumber > secretNumber) {
     if (score > 1) {
       document.querySelector('.guess-message').textContent =
