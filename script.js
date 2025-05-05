23f5ea6baa492ba27399a0094106343b9aadb8a0
Igor <izelenov7@mail.ru>
2025-05-05 20:38:06 +0300

    end beta version

--- script.js
+++ script.js
@@ -2,28 +2,30 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let result = 1;
+let result = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
-  console.log(guessingNumber);
 
   if (!guessingNumber) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
   } else if (guessingNumber === secretNumber) {
     document.querySelector('.guess-message').textContent = 'Вы угадали!';
     document.querySelector('.question').textContent = secretNumber;
-    document.querySelector('.highscore').textContent = result;
     document.querySelector('.question').style.backgroundColor = 'green';
     document.querySelector('header').style.borderBottom = '7px solid green';
     document.querySelector('h1').textContent = 'Победа!';
     document.querySelector('h1').style.fontSize = '6rem';
+
+    if (score > result) {
+      result = score;
+      document.querySelector('.highscore').textContent = result;
+    }
   } else if (guessingNumber > secretNumber) {
     if (score > 1) {
       document.querySelector('.guess-message').textContent =
         'Число слишком большое';
       score--;
-      result++;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.guess-message').textContent = 'Игра окончена.';
@@ -34,7 +36,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.guess-message').textContent =
         'Число слишком маленькое';
       score--;
-      result++;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.guess-message').textContent = 'Игра окончена.';
@@ -44,11 +45,12 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.question').textContent = '???';
   document.querySelector('.question').style.backgroundColor = 'white';
   document.querySelector('.guess-message').textContent = 'Начни угадывать!';
-  document.querySelector('.score').textContent = '20';
-  document.querySelector('.highscore').textContent = '0';
   document.querySelector('h1').textContent = 'Угадай Число!';
   document.querySelector('h1').style.fontSize = '3rem';
   document.querySelector('header').style.borderBottom = '7px solid #fff';
