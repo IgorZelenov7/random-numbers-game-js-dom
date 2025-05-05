5182153f987b05b8ef2a02808a754d9f5266962f
Igor <izelenov7@mail.ru>
2025-05-05 14:37:47 +0300

    add frontend project

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20);
+console.log(secretNumber);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessingNumber = Number(document.querySelector('.number-input').value);
+  console.log(guessingNumber);
+
+  if (!guessingNumber) {
+    document.querySelector('.guess-message').textContent = 'Введите число!';
+  }
+});
