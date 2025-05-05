d3048c3cbf1f2601d42929d02202fd5eadebe352
Igor <izelenov7@mail.ru>
2025-05-05 14:58:54 +0300

    add game logic | score, number variants

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
+let score = 20;
+
+document.querySelector('.question').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
@@ -9,5 +11,17 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guessingNumber) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
+  } else if (guessingNumber === secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Вы угадали!';
+  } else if (guessingNumber > secretNumber) {
+    document.querySelector('.guess-message').textContent =
+      'Число слишком большое';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guessingNumber < secretNumber) {
+    document.querySelector('.guess-message').textContent =
+      'Число слишком маленькое';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
